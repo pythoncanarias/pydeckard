0a2b12b8253591bcb0ea0d9a054f5cf8be25e62e
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-12-03 22:48:37 +0000

    Added welcome message

--- bot.py
+++ bot.py
@@ -2,17 +2,19 @@ import config
 from telegram.ext import Updater, Filters, MessageHandler
 
 
-def check_bots(bot, update):
+def welcome(bot, update):
     new_member = update.message.new_chat_members[0]
     if new_member.is_bot:
         msg = f"@{new_member.username} is a bot!! -> Remove it ❌"
-        bot.send_message(chat_id=update.message.chat_id, text=msg)
+    else:
+        msg = f"Welcome {new_member.name}!! 😀"
+    bot.send_message(chat_id=update.message.chat_id, text=msg)
 
 
 updater = Updater(config.TELEGRAM_BOT_TOKEN)
 
 updater.dispatcher.add_handler(
-    MessageHandler(Filters.status_update.new_chat_members, check_bots)
+    MessageHandler(Filters.status_update.new_chat_members, welcome)
 )
 
 
