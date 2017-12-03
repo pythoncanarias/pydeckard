f56b534c6de52faef68204175ff37d7f278a6e8d
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-12-03 22:22:38 +0000

    Added filter when new chat members arrive

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
 import config
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, Filters, MessageHandler
 
 
-def start(bot, update):
-    update.message.reply_text("Hello World!")
-
-
-def hello(bot, update):
-    update.message.reply_text(
-        "Hello {}".format(update.message.from_user.first_name)
-    )
+def check_bots(bot, update):
+    new_member = update.message.new_chat_members[0]
+    if new_member.is_bot:
+        msg = f"@{new_member.username} is a bot!! -> Remove it ❌"
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
 
 
 updater = Updater(config.TELEGRAM_BOT_TOKEN)
 
-updater.dispatcher.add_handler(CommandHandler("start", start))
-updater.dispatcher.add_handler(CommandHandler("hello", hello))
+updater.dispatcher.add_handler(
+    MessageHandler(Filters.status_update.new_chat_members, check_bots)
+)
+
 
 updater.start_polling()
 updater.idle()
