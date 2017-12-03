31c7667479612f4d09603bf96116454ffa3d256a
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-12-03 19:37:57 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PyDeckard
+
+> "He visto cosas que vosotros no creeríais" En honor a Rick Deckard de Blade Runner.
+
+Bot de Telegram hecho en Python para automatizar distintas tareas.
---
+++ bot.py
@@ -0,0 +1,21 @@
+import config
+from telegram.ext import Updater, CommandHandler
+
+
+def start(bot, update):
+    update.message.reply_text("Hello World!")
+
+
+def hello(bot, update):
+    update.message.reply_text(
+        "Hello {}".format(update.message.from_user.first_name)
+    )
+
+
+updater = Updater(config.TELEGRAM_BOT_TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler("start", start))
+updater.dispatcher.add_handler(CommandHandler("hello", hello))
+
+updater.start_polling()
+updater.idle()
