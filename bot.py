4104cb96fd545ace6de4054baa708bd9a0eae358
mrroot5 <mrroot5@protonmail.com>
2024-07-18 14:06:57 +0200

    Bump python-telegram.bot to 21.4

--- bot.py
+++ bot.py
@@ -2,47 +2,36 @@ import logging
 from time import sleep
 
 import telegram
-from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
 from telegram import Update
+from telegram.ext import ApplicationBuilder, filters, MessageHandler, CommandHandler, ContextTypes
 
 import config
 import utils
 
 
-logger = logging.getLogger('bot')
-
-
-def command_start(update, context):
+async def command_start(update, context):
     logger.info('Received command /start')
-    context.bot.send_message(chat_id=update.message.chat_id, text=config.BOT_GREETING)
+    await update.message.reply_text(config.BOT_GREETING)
 
 
-def command_help(update, context):
+async def command_help(update, context):
     logger.info('Received command /help')
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Available commands:\n"
-        " - /start - start intereaction with the bot\n"
-        " - /help - Show commands\n"
-        " - /status - Show status and alive time\n"
-        )
+    await update.message.reply_text("Available commands:\n"
+                                    " - /start - start intereaction with the bot\n"
 
 
-def command_status(update, context):
+async def command_status(update, context):
     logger.info('bot asked to execute /status commamd')
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=f'Status is OK, running since {utils.since()}',
-    )
+    await update.message.reply_text(f'Status is OK, running since {utils.since()}',)
 
 
-def welcome(update: Update, context):
+async def welcome(update: Update, context):
     logger.info('Received new user event')
     new_member = update.message.new_chat_members[0]
 
     logger.info(f'Waiting {config.WELCOME_DELAY} seconds until user completes captcha...')
     sleep(config.WELCOME_DELAY)
-    membership_info = context.bot.get_chat_member(update.message.chat_id, new_member.id)
+    membership_info = await context.bot.get_chat_member(update.message.chat_id, new_member.id)
     if membership_info['status'] == 'left':
         logger.info(f'Skipping welcome message, user {new_member.name} is no longer in the chat')
         return
@@ -51,27 +40,23 @@ def welcome(update: Update, context):
     msg = None
 
     if new_member.is_bot:
-        msg = f"{new_member.name} is a *bot*!! " \
+        msg = f"{new_member.name} is a *bot*\!\! " \
               "-> It could be kindly removed 🗑"
     else:
         if utils.is_bot(new_member):
-            context.bot.delete_message(update.message.chat_id,
+            await context.bot.delete_message(update.message.chat_id,
                                        update.message.message_id)
-            if context.bot.kick_chat_member(update.message.chat_id, new_member.id):
+            if await context.bot.kick_chat_member(update.message.chat_id, new_member.id):
                 msg = (f"*{new_member.username}* has been banned because I "
                        "considered it was a bot. ")
         else:
-            msg = f"Welcome {new_member.name}!! " \
+            msg = f"Welcome {new_member.name}\!\! " \
                    "I am a friendly and polite *bot* 🤖"
     if msg:
-        context.bot.send_message(
-            chat_id=update.message.chat_id,
-            text=msg,
-            parse_mode=telegram.ParseMode.MARKDOWN
-        )
+        await update.message.reply_text(msg, parse_mode=telegram.constants.ParseMode('MarkdownV2'))
 
 
-def reply(update, context):
+async def reply(update, _context):
     if not config.bot_replies_enabled():
         return
 
@@ -79,6 +64,7 @@ def reply(update, context):
     reply_spec = utils.triggers_reply(msg) if msg else None
     if reply_spec is not None:
         logger.info(f'bot sends reply {reply_spec.reply}')
+        await update.message.reply_text(reply_spec.reply)
         context.bot.send_message(
             chat_id=update.message.chat_id,
             text=reply_spec.reply
@@ -92,19 +78,19 @@ def main():
         )
     logger.info('Starting bot...')
     config.log(logger.info)
-    updater = Updater(config.TELEGRAM_BOT_TOKEN)
-    dp = updater.dispatcher
+    app = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
 
-    dp.add_handler(CommandHandler('start', command_start))
-    dp.add_handler(CommandHandler('help', command_help))
-    dp.add_handler(CommandHandler('status', command_status))
-    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members,
-                                  welcome, run_async=True))
-    dp.add_handler(MessageHandler(Filters.chat_type.groups, reply))
+    app.add_handler(CommandHandler('start', command_start))
+    app.add_handler(CommandHandler('help', command_help))
+    app.add_handler(CommandHandler('status', command_status))
+    app.add_handler(MessageHandler(
+        filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
+    app.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND & filters.ChatType.GROUPS, reply))
 
     logger.info('Bot is ready')
-    updater.start_polling(poll_interval=config.POLL_INTERVAL)
-    updater.idle()
+    app.run_polling(allowed_updates=Update.ALL_TYPES,
+                    poll_interval=config.POLL_INTERVAL)
 
 
 if __name__ == "__main__":
